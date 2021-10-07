54908efb3c36bc630d5ae823261760a4969c519c
Yasith Nethsara <89378001+YasithNethsara@users.noreply.github.com>
2021-10-07 13:20:17 +0530

    Update FeedbackRepository.java

--- src/main/java/com/example/demo/repository/FeedbackRepository.java
+++ src/main/java/com/example/demo/repository/FeedbackRepository.java
@@ -7,6 +7,16 @@ import org.springframework.stereotype.Repository;
 import com.example.demo.model.Feedback;
 
 /* feedback repository*/
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+	@Query("SELECT u FROM User u WHERE u.id LIKE ?1")
+	public List<User> findAll(Long keyword);
+	
+	
+	//JRE file take a JPAREPOSITORY , its take to user entity  & primary key. Automatically generate
+
+	
+}
 
 
 
